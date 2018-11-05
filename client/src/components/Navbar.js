a513ddaf043058df8968416eda48ec890afd20a5
Binh Tran <thanhbinh.tran93@gmail.com>
2018-11-05 18:37:55 +0200

    Add route, navbar, and basic components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, Text, Heading, Image } from 'gestalt';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => (
+    <Box display="flex" alignItems="center" justifyContent="around" height={70} color="midnight" padding={1} shape="roundedBottom">
+        {/* Sign In Link */}
+        <NavLink activeClassName="active" to="/signin">
+            <Text size="xl" color="white">
+                Sign In
+            </Text>
+        </NavLink>
+
+        {/* Title and Logo */}
+        <NavLink activeClassName="active" exact to="/">
+            <Box display="flex" alignItems="center">
+                <Box height={50} width={50} margin={2}>
+                    <Image src="./icons/logo.svg" alt="Shop Logo" naturalHeight={1} naturalWidth={1} />
+                </Box>
+                <Heading size="xs" color="orange">
+                    Shop Name
+                </Heading>
+            </Box>
+        </NavLink>
+
+        {/* Sign Up Link */}
+        <NavLink activeClassName="active" to="/signup">
+            <Text size="xl" color="white">
+                Sign Up
+            </Text>
+        </NavLink>
+    </Box>
+)
+
+export default Navbar;
