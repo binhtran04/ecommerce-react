52b074c23dc8e9be9cbf84413dfdd0d9af92295e
Binh Tran <thanhbinh.tran93@gmail.com>
2018-11-06 18:10:29 +0200

    Add Strapi server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Container, Box, Heading } from 'gestalt';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Container>
+        {/* Brans Section */}
+        <Box display="flex" justifyContent="center" marginBottom={2}>
+          <Heading color="midnight" size="md">
+            Brew Brands
+          </Heading>
+        </Box>
+      </Container>
     );
   }
 }
