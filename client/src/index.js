a068091ef5afad3d2c2a8ae5f162ac2612f38b2b
Binh Tran <thanhbinh.tran93@gmail.com>
2018-11-08 22:20:57 +0200

    add Brews component

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import Navbar from './components/Navbar';
+import Brews from './components/Brews';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -19,6 +20,7 @@ const Root = () => (
                 <Route component={Signin} path="/signin" />
                 <Route component={Signup} path="/signup" />
                 <Route component={Checkout} path="/checkout" />
+                <Route component={Brews} path="/:brandId" />
                 <Route component={App} path="/" />
             </Switch>
         </React.Fragment>
